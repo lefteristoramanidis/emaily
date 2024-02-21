37d1292f2e5183c8699e00994b23d2cc412e932d
lefteristoramanidis <73037967+lefteristoramanidis@users.noreply.github.com>
2024-02-21 11:38:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
 
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
